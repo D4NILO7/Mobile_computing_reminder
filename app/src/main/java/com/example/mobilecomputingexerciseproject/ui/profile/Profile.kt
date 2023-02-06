1fbaa99529b51391827abcd056590bf548751458
Daniel Tisza <daniel.tisza@student.tuke.sk>
2023-02-06 03:43:12 +0200

    profile good

--- app/src/main/java/com/example/mobilecomputingexerciseproject/ui/profile/Profile.kt
+++ app/src/main/java/com/example/mobilecomputingexerciseproject/ui/profile/Profile.kt
@@ -1,27 +1,70 @@
 package com.example.mobilecomputingexerciseproject.ui.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.toColorInt
 import androidx.navigation.NavController
+import com.example.mobilecomputingexerciseproject.R
 
 
 @Composable
 fun Profile(
     navController: NavController
 ){
-    Surface{
+    Surface(modifier = Modifier.fillMaxHeight()){
         Column(
-            modifier = Modifier
-                .fillMaxSize()
+
         ) {
-            CreateTopBar(navController = navController )
+            CreateTopBar(navController = navController)
+            Column (
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 96.dp)
+                    .fillMaxSize()
+                    .background(Color(0xFF3E4445)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+
+                Image(
+                    painter = painterResource(id = R.drawable.profile_picture),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(CircleShape),
+                )
+
+                Spacer(modifier = Modifier.height(18.dp))
+                Row(
+                ) {
+                    Text("Daniel ", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                    Text(" Tisza", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                }
+                Text("@Danilo004", fontSize = 24.sp)
+                Spacer(modifier = Modifier.height(32.dp))
+                Text("danieltisza23@gmail.com", fontSize = 24.sp)
+
+            }
+
         }
     }
 }
@@ -32,7 +75,7 @@ fun CreateTopBar(
 ){
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
 
     ) {
         TopAppBar(
@@ -50,14 +93,14 @@ fun CreateTopBar(
                     )
                 } },
             actions = {
-                Text(text = "Log Out", fontWeight = FontWeight.Bold, color = Color.Red)
                 IconButton(
                     onClick = {navController.navigate("login")}
                 ){
                     Icon(
                         imageVector = Icons.Default.ExitToApp,
                         tint = Color.Red,
-                        contentDescription = null
+                        contentDescription = null,
+                        modifier = Modifier.size(40.dp)
                     )
                 }
 
